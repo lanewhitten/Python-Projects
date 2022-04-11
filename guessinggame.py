0190ce6c09756ccd73303039b99cdbe8c5949b6b
Lane Whitten <lanetron@outlook.com>
2022-04-10 19:25:20 -0500

    A Simple Number Guessing Game

---
+++ guessinggame.py
@@ -0,0 +1,37 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print("Too Low!")
+        elif guess > random_number:
+            print("Too High!")
+    print(f"You guessed it congrats {random_number}")
+
+def computer_guess(x):
+    low = 1
+    high = x    
+    feedback = ""
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C) '.lower())
+        if feedback == 'h':
+            high = guess - 1 
+        elif feedback == 'l':
+            low = guess + 1 
+
+    print(f'Yay the pc guessed your number, {guess}, correctly!')
+
+select = input(f"Do you want to guess (i) or do you want the computer to guess (c)?".lower())
+if select == "c":
+    computer_guess(1000)
+elif select == "i":
+    guess(1000)
+
+
